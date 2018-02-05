8eead4640773bf698e5585c49a3823095903d632
Sergei Zhgirovski <ixanezis@yandex-team.ru>
2018-02-05 13:12:03 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-#!/bin/sh -eu
+#!/bin/bash -eu
 
 shopt -s dotglob
 rm -rf $HOME/.vim
+rm -rf $HOME/.git
+rm -rf $HOME/dircolors-solarized
+cd $HOME/configs
+git submodule foreach --recursive '[ -f .git ] && echo "gitdir: $(realpath --relative-to=. $(cut -d" " -f2 .git))" > .git' >/dev/null
+cd $HOME
 mv $HOME/configs/* $HOME/
 rm -rf $HOME/configs
-cd $HOME
